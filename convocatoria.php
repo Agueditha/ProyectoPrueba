c260ea2fec688dbfd8d0d0057c84d82a8ee55512
Agueditha <agueda.92@hotmail.com>
2013-10-29 20:30:07 -0600

    sdfshfj

---
+++ convocatoria.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang = "es-MX">
+	<meta charset = "UTF-8">
+	<head>
+		<title>Convocatorias</title>
+	<img align="right" src="https://dl.dropboxusercontent.com/u/38526282/Proyectos/images/c-convocatoria.png">
+	</head>
+	<body></body>
+</html>
---
+++ help.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang = "es-MX">
+	<meta charset = "UTF-8">
+	<head>
+		<title>Ayuda</title>
+	</head>
+	<center><img src="C:\Users\Jazu\Documents\UV\Admon Proyectos\Proyecto\images\c-ayuda.png"></center>
+	<body>
+	</body>
+</html>
