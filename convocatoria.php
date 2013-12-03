81a14ab8c82f78e1f0bc0d4ebd8e54e8b4d826f2
Jazmin <kyo.713@hotmail.com>
2013-12-03 12:49:49 -0600

    Delete convocatoria.php

--- convocatoria.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang = "es-MX">
-	<meta charset = "UTF-8">
-	<head>
-		<title>Convocatorias</title>
-	<img align="right" src="https://dl.dropboxusercontent.com/u/38526282/Proyectos/images/c-convocatoria.png">
-	</head>
-	<body></body>
-</html>
